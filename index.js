95ef53f577e009ccb30e383386299a8f48b67a95
Pritish <118997899+Pritish-Pradhan@users.noreply.github.com>
2024-05-03 21:53:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//function to blur the background on clicking navbar
 $("nav button").on("click",function(){
     if($("#mainContainer").hasClass("Blur")){
         $("#mainContainer").removeClass("Blur");
@@ -5,4 +6,16 @@ $("nav button").on("click",function(){
     else{
         $("#mainContainer").addClass("Blur");
     }
+});
+
+//function for removing nav on clicking elsewhere
+$("#mainContainer").on("click",function(){
+    if($("#mainContainer").hasClass("Blur")){
+        $("nav button").click();
+    }
+});
+
+//function to close nav on clicking a item in it
+$(".nav-item").on("click",function(){
+    $("nav button").click();
 });
